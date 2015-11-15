1c13eb4333ecc77703926de03dc29b5852d131b6
Masayuki Ozawa <masayuki.ozawa@live.jp>
2015-11-15 22:50:40 +0900

    デッドロックチェーンの情報を追加

--- xEvents/拡張イベントのベーステンプレート.sql
+++ xEvents/拡張イベントのベーステンプレート.sql
@@ -8,13 +8,15 @@
 -- 統計が設定されていない列に対して実行されたクエリ
 -- ソート時にメモリ不足が発生したクエリ
 -- 実行に 10 秒以上かかったクエリ
--- デッドロックレポート
+-- デッドロックレポート/デッドロックチェーン
 /********************************************************************************/
-CREATE EVENT SESSION [Query Trace] ON SERVER 
+CREATE EVENT SESSION [Query_Trace] ON SERVER 
 ADD EVENT sqlserver.blocked_process_report,
 ADD EVENT sqlserver.execution_warning(SET collect_server_memory_grants=(1)
     ACTION(sqlserver.sql_text)),
 ADD EVENT sqlserver.hash_warning(
+    ACTION(sqlserver.query_hash,sqlserver.query_plan_hash,sqlserver.sql_text)),
+ADD EVENT sqlserver.lock_deadlock_chain(SET collect_database_name=(1),collect_resource_description=(1)
     ACTION(sqlserver.query_hash,sqlserver.query_plan_hash,sqlserver.sql_text)),
 ADD EVENT sqlserver.missing_column_statistics(SET collect_column_list=(1)
     ACTION(sqlserver.sql_text)),
@@ -22,6 +24,9 @@ ADD EVENT sqlserver.sort_warning(
     ACTION(sqlserver.query_hash,sqlserver.query_plan_hash,sqlserver.sql_text)),
 ADD EVENT sqlserver.sql_batch_completed(SET collect_batch_text=(1)
     WHERE ([duration]>=(10000000))),
-ADD EVENT sqlserver.xml_deadlock_report
+ADD EVENT sqlserver.xml_deadlock_report(
+    ACTION(sqlserver.sql_text))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
 GO
+
+

52409758cb7d7f20991c29d44dcfda6a6fee3c12
Masayuki Ozawa <masayuki.ozawa@live.jp>
2016-07-10 11:43:30 +0900

    インデックスの再構築を SQL Database に対応し、クエリの進行状況取得を追加

--- Manage/インデックスのメンテナンス/インデックスの再構築.sql
+++ Manage/インデックスのメンテナンス/インデックスの再構築.sql
@@ -1,19 +1,23 @@
-DECLARE @SchemaName sysname, @TableName sysname, @IndexName sysname, @IndexType tinyint, @PartitionNumber int, @PartitionCount int, @Fragment float, @PageCount bigint, @IncludeColumnstore bit
+﻿DECLARE @SchemaName sysname, @TableName sysname, @IndexName sysname, @IndexType tinyint, @PartitionNumber int, @PartitionCount int, @Fragment float, @PageCount bigint, @IncludeColumnstore bit
 DECLARE @basesql nvarchar(max), @sql nvarchar(max) 
 DECLARE @Edition nvarchar(max)
 
 DECLARE @PageThreshold int = 1000			-- メンテナンス対象とするページの数の閾値
-DECLARE @RebuildThreshold int = 30			-- REBUILD の閾値
 DECLARE @ReorganizeThreshold int = 15		-- REORGANIZE の閾値
+DECLARE @RebuildThreshold int = 30			-- REBUILD の閾値
 DECLARE @OnlineOnly bit = 1					--  0 : Enterprise Edition の REBUILD で ONLINE = ON が使用できない構成でも再構築を実施
 
+DECLARE @StartDate AS datetime2 = (SELECT SYSDATETIME())
+
+PRINT 'Index Maintenance Start:' + CAST(@StartDate AS sysname)
 
 SET @Edition = CONVERT(nvarchar, SERVERPROPERTY('Edition'))
 
 -- Live Query 統計が取得できるよう、PROFILE ON を実施
 SET @basesql = 'SET STATISTICS PROFILE ON;ALTER INDEX @1 On @2.@3 REBUILD @4 @5'
 
-IF PATINDEX('%Enterprise%', @Edition) > 0 
+-- Enterprise と SQL Database はオンラインでのインデックス再構築を基本とする
+IF PATINDEX('%Enterprise%', @Edition) > 0 or PATINDEX('%SQL Azure%', @Edition) > 0
     SET @basesql = REPLACE(@basesql, '@5', 'WITH (ONLINE=ON)') 
 ELSE 
     SET @basesql = REPLACE(@basesql, '@5', '')
@@ -56,7 +60,10 @@ OUTER APPLY
 )AS c(include_columnstore)
 OUTER APPLY
 (
-	-- SQL Server 2014 以前の場合は、In-Memory OLTP の判断は不要なため、本ブロックは SELECT NULL のみとする
+	/*
+	SQL Server 2014 より前のバージョンは、In-Memory OLTP の判断ができないため、本ブロックは SELECT NULL のみに入れ替える
+	SELECT NULL
+	*/
 	SELECT
 		1
 	FROM
@@ -85,10 +92,10 @@ INTO @SchemaName, @TableName, @IndexName, @IndexType, @PartitionNumber, @Partiti
 
 WHILE @@FETCH_STATUS = 0 
 BEGIN 
-	PRINT @SchemaName + ':' + @TableName + ':' + @IndexName + ':' +  CAST(@IndexType AS nvarchar(1)) + ':' +  CAST(@PartitionNumber AS nvarchar(3)) + ':' +  CAST(@PartitionCount AS nvarchar(3)) + ':' + CAST(@IncludeColumnstore AS nvarchar(1)) + ':' +  CAST(CAST(@Fragment AS int) AS nvarchar(3)) + ':' +  CAST(@PageCount AS nvarchar(255))
+	PRINT @SchemaName + '.' + @TableName + ':' + @IndexName + ':' +  CAST(@IndexType AS nvarchar(1)) + ':' +  CAST(@PartitionNumber AS nvarchar(3)) + ':' +  CAST(@PartitionCount AS nvarchar(3)) + ':IncludeColumnstore=' + CAST(@IncludeColumnstore AS nvarchar(1)) + ':Fragment=' +  CAST(CAST(@Fragment AS int) AS nvarchar(3)) + ':Page Count=' +  CAST(@PageCount AS nvarchar(255))
 	-- 断片化が 15% 以内の場合かつ、1,000 ページ以下の場合、メンテナンスは実施しない
 	-- 列ストアインデックスについては、断片化 / ページカウントが 0 となるため、本スクリプトでは再構築は行われない
-	IF @Fragment > @ReorganizeThreshold AND @PageCount  > @PageThreshold
+	IF @Fragment > @ReorganizeThreshold AND @PageCount > @PageThreshold
 	BEGIN
 		SET @sql = REPLACE(@basesql,	'@1', QUOTENAME(@IndexName)) 
 		SET @sql = REPLACE(@sql,		'@2', QUOTENAME(@SchemaName)) 
@@ -132,3 +139,6 @@ END
 
 CLOSE IXC 
 DEALLOCATE IXC
+
+PRINT 'Index Maintenance End:' + CAST(SYSDATETIME() AS sysname)
+PRINT 'Total Time (Sec):' + CAST(DATEDIFF(ss, @StartDate, SYSDATETIME()) AS sysname)
---
+++ Query/クエリの進行状況を取得.sql
@@ -0,0 +1,28 @@
+﻿-- SET STATISTICS PROFILE ON を有効にしているクエリに関して情報を取得
+SELECT
+    qp.session_id, 
+    qp.request_id, 
+    t.text,
+    --p.query_plan,
+    ot.task_state,
+    qp.physical_operator_name, 
+    qp.node_id, 
+    qp.thread_id, 
+    qp.row_count,
+    qp.estimate_row_count,
+    CASE row_count
+        WHEN 0 THEN 0.0
+        ELSE convert(float,qp.row_count) / qp.estimate_row_count * 100.0
+    END as progress
+FROM
+    sys.dm_exec_query_profiles qp
+    CROSS APPLY
+    sys.dm_exec_sql_text (sql_handle) t
+    CROSS APPLY
+    sys.dm_exec_query_plan(plan_handle) p
+    LEFT JOIN
+    sys.dm_os_tasks ot
+ON
+    qp.task_address = ot.task_address
+ORDER BY
+    session_id, request_id, node_id, thread_id

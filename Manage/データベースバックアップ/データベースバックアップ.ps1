a72db75dc2620bf7d0a5699cc31f7433699fe474
Masayuki Ozawa <masayuki.ozawa@live.jp>
2016-08-01 11:48:58 +0900

    インスタンスの指定方法の見直し/バックアップパス,整合性検証の追加

--- Manage/データベースバックアップ/データベースバックアップ.ps1
+++ Manage/データベースバックアップ/データベースバックアップ.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 複数のバージョンの SQLPS をインストールしており、'InputObject' をバインドできません。"のエラーが発生する場合、以下の実施を検討する
 http://dba.stackexchange.com/questions/102682/backup-sqldatabase-cannot-bind-parameter-inputobject-error-being-thrown
 http://powershelldiaries.blogspot.in/2015/08/backup-sqldatabase-restore-sqldatabase.html
@@ -15,6 +15,14 @@ $env:PSModulePath = ($TempArray -notmatch '110') -join ';'
 
 #>
 
+Push-Location
+Import-Module SQLPS -DisableNameChecking
+Pop-Location
+
+$global:VerifySQL = @"
+RESTORE VERIFYONLY FROM DISK=N'{0}'
+"@
+
 function Start-SQLServerFullBackup(){
 <#
 .DESCRIPTION
@@ -22,19 +30,36 @@ SQL Server の完全バックアップを取得
 #>
 [CmdletBinding()]
 param(
-    [string]$Instance = $env:COMPUTERNAME
+    [string]$Instance = $env:COMPUTERNAME,
+    [string]$BackupPath = "",
+    [switch]$NoVerify
 )
-
-    Import-Module SQLPS -DisableNameChecking
-
     $Server = New-Object Microsoft.SqlServer.Management.Smo.Server $Instance
     $Server.ConnectionContext.StatementTimeout = 0
 
-    foreach($Database in $Server.Databases){
-        # tempdb はバックアップを取得できないためスキップ
-        if($Database.Name -ne "tempdb"){
-            Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile ("{0}-{1}-{2}_FULL.bak" -f $env:COMPUTERNAME, $Database.Name,(Get-Date).ToString("yyyyMMdd")) -BackupAction Database -CompressionOption On -Checksum -ContinueAfterError
-            # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_FULL.bak' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
+    foreach($Database in ($Server.Databases | ? Status -eq "Normal")){
+        try{
+            # tempdb はバックアップを取得できないためスキップ
+            if($Database.Name -ne "tempdb"){
+                $BackupFile =  ("{0}-{1}-{2}_FULL.bak" -f $Instance, $Database.Name,(Get-Date).ToString("yyyyMMdd"))
+
+                if ($BackupPath -ne ""){
+                    $BackupFile = Join-Path -Path $BackupPath -ChildPath $BackupFile
+                }
+
+                Write-Output ("{0} [{1}] 完全バックアップ取得開始" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile $BackupFile -BackupAction Database -CompressionOption On -Checksum -ContinueAfterError -ErrorAction Stop
+                # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_FULL.bak' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR{
+                Write-Output ("{0} [{1}] 完全バックアップ取得終了" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+
+                if(!($NoVerify)){
+                    Write-Output ("{0} [{1}] バックアップ整合性検証開始" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                    Invoke-Sqlcmd -ServerInstance $Instance -Query ($VerifySQL -f $BackupFile) -QueryTimeout 1200 -ErrorAction Stop
+                    Write-Output ("{0} [{1}] バックアップ整合性検証終了" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                }
+            }
+        }catch{
+            throw $_
         }
     }
 }
@@ -46,10 +71,10 @@ SQL Server の差分バックアップを取得
 #>
 [CmdletBinding()]
 param(
-    [string]$Instance = $env:COMPUTERNAME
+    [string]$Instance = $env:COMPUTERNAME,
+    [string]$BackupPath = "",
+    [switch]$NoVerify
 )
-    Import-Module SQLPS -DisableNameChecking
-
     $Server = New-Object Microsoft.SqlServer.Management.Smo.Server $Instance
     $Server.ConnectionContext.StatementTimeout = 0
 
@@ -62,24 +87,46 @@ and
 db_id(database_name) = DB_ID('{1}')
 "@
 
-    foreach($Database in $Server.Databases){
-        # master は差分バックアップが取得できないため毎回完全バックアップを取得
-        if ($Database.Name -eq "master"){
-            Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile ("{0}-{1}-{2}_DIFF.bak" -f $env:COMPUTERNAME, $Database.Name,(Get-Date).ToString("yyyyMMdd")) -BackupAction Database -CompressionOption On -Checksum -ContinueAfterError
-            # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_FULL.bak' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
-        # tempdb はバックアップを取得できないためスキップ
-        }elseif ($Database.name -ne "tempdb"){
-            # 完全バックアップの取得状況を確認
-            $FullBackupCount = Invoke-Sqlcmd -ServerInstance $env:COMPUTERNAME -Query ($BackupCheckSQL -f "D", $Database.Name)
+    foreach($Database in ($Server.Databases | ? Status -eq "Normal")){
+        try{
+            $BackupFile =  ("{0}-{1}-{2}_DIFF.bak" -f $Instance, $Database.Name,(Get-Date).ToString("yyyyMMdd"))
 
-            if($FullBackupCount.count -eq 0){
-                Write-Output ("{0} の完全バックアップが取得されていないため、初回完全バックアップを取得します" -f $Database.Name)
-                Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile ("{0}-{1}-{2}_DIFF.bak" -f $env:COMPUTERNAME, $Database.Name,(Get-Date).ToString("yyyyMMdd")) -BackupAction Database -CompressionOption On -Checksum -ContinueAfterError
-                # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_FULL.bak' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
-            }else{
-                Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile ("{0}-{1}-{2}_DIFF.bak" -f $env:COMPUTERNAME, $Database.Name,(Get-Date).ToString("yyyyMMdd")) -BackupAction Database -CompressionOption On -Checksum -Incremental -ContinueAfterError
-                # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_DIFF.bak' WITH  DIFFERENTIAL , NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
+            if ($BackupPath -ne ""){
+                $BackupFile = Join-Path -Path $BackupPath -ChildPath $BackupFile
             }
+            # master は差分バックアップが取得できないため毎回完全バックアップを取得
+            if ($Database.Name -eq "master"){
+                Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile $BackupFile -BackupAction Database -CompressionOption On -Checksum -ContinueAfterError -ErrorAction Stop
+                # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_FULL.bak' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
+                if(!($NoVerify)){
+                     Invoke-Sqlcmd -ServerInstance $Instance -Query ($VerifySQL -f $BackupFile) -QueryTimeout 1200
+                }
+            # tempdb はバックアップを取得できないためスキップ
+            }elseif ($Database.name -ne "tempdb"){
+                # 完全バックアップの取得状況を確認
+                $FullBackupCount = Invoke-Sqlcmd -ServerInstance $Instance -Query ($BackupCheckSQL -f "D", $Database.Name)  -QueryTimeout 120
+
+                if($FullBackupCount.count -eq 0){
+                    Write-Output ("{0} の完全バックアップが取得されていないため、初回完全バックアップを取得します" -f $Database.Name)
+                    Write-Output ("{0} [{1}] 完全バックアップ取得開始" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                    Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile $BackupFile -BackupAction Database -CompressionOption On -Checksum -ContinueAfterError -ErrorAction Stop
+                    # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_FULL.bak' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
+                    Write-Output ("{0} [{1}] 完全バックアップ取得終了" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                }else{
+                    Write-Output ("{0} [{1}] 差分バックアップ取得開始" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                    Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile $BackupFile -BackupAction Database -CompressionOption On -Checksum -Incremental -ContinueAfterError -ErrorAction Stop
+                    # BACKUP DATABASE [xxxx] TO  DISK = N'xxxx-xxxx-yyyymmdd_DIFF.bak' WITH  DIFFERENTIAL , NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
+                    Write-Output ("{0} [{1}] 差分バックアップ取得終了" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                }
+
+                if(!($NoVerify)){
+                    Write-Output ("{0} [{1}] バックアップ整合性検証開始" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                    Invoke-Sqlcmd -ServerInstance $Instance -Query ($VerifySQL -f $BackupFile) -QueryTimeout 1200 -ErrorAction Stop
+                    Write-Output ("{0} [{1}] バックアップ整合性検証終了" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                }
+            }
+        }catch{
+            throw $_
         }
     }
 }
@@ -91,11 +138,10 @@ SQL Server のログバックアップを取得
 #>
 [CmdletBinding()]
 param(
-    [string]$Instance = $env:COMPUTERNAME
+    [string]$Instance = $env:COMPUTERNAME,
+    [string]$BackupPath = "",
+    [switch]$NoVerify
 )
-
-    Import-Module SQLPS -DisableNameChecking
-    
     $Server = New-Object Microsoft.SqlServer.Management.Smo.Server $Instance
     $Server.ConnectionContext.StatementTimeout = 0
 
@@ -108,25 +154,37 @@ and
 db_id(database_name) = DB_ID('{1}')
 "@
 
-    foreach($Database in $Server.Databases){
-        # 単純復旧モデルはログバックアップをスキップ
-        if ($database.RecoveryModel -ne "Simple"){
-            # 完全バックアップの取得状況を確認
-            $FullBackupCount = Invoke-Sqlcmd -ServerInstance $env:COMPUTERNAME -Query ($BackupCheckSQL -f "D", $Database.Name)
+    foreach($Database in ($Server.Databases | ? Status -eq "Normal")){
+        try{
+            $BackupFile =  ("{0}-{1}-{2}_Log.trn" -f $Instance, $Database.Name,(Get-Date).ToString("yyyyMMdd"))
 
-            # 一度も完全バックアップを取得していない場合はログバックアップを取得できないためスキップ
-            if ($FullBackupCount.count  -eq 0) {
-                Write-Output ("{0} の完全バックアップが取得されていないため、ログバックアップの取得をスキップします" -f $database.Name)
-            }else{
-                Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile ("{0}-{1}-{2}_LOG.trn" -f $env:COMPUTERNAME, $Database.Name,(Get-Date).ToString("yyyyMMdd"))  -BackupAction Log  -CompressionOption On -Checksum -ContinueAfterError
-                # BACKUP LOG [xxxxx] TO  DISK = N'xxxxx-xxxx-yyyymmdd_LOG.trn' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
+            if ($BackupPath -ne ""){
+                $BackupFile = Join-Path -Path $BackupPath -ChildPath $BackupFile
             }
+
+            # 単純復旧モデルはログバックアップをスキップ
+            if ($database.RecoveryModel -ne "Simple"){
+                # 完全バックアップの取得状況を確認
+                $FullBackupCount = Invoke-Sqlcmd -ServerInstance $Instance -Query ($BackupCheckSQL -f "D", $Database.Name) -QueryTimeout 120
+
+                # 一度も完全バックアップを取得していない場合はログバックアップを取得できないためスキップ
+                if ($FullBackupCount.count  -eq 0) {
+                    Write-Output ("{0} の完全バックアップが取得されていないため、ログバックアップの取得をスキップします" -f $database.Name)
+                }else{
+                    Write-Output ("{0} [{1}] ログバックアップ取得開始" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                    Backup-SqlDatabase -InputObject $Server -Database $Database.Name -BackupFile $BackupFile -BackupAction Log  -CompressionOption On -Checksum -ContinueAfterError  -ErrorAction Stop
+                    # BACKUP LOG [xxxxx] TO  DISK = N'xxxxx-xxxx-yyyymmdd_LOG.trn' WITH NOFORMAT, NOINIT, NOSKIP, REWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM, CONTINUE_AFTER_ERROR
+                    Write-Output ("{0} [{1}] ログバックアップ取得終了" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+
+                    if(!($NoVerify)){
+                        Write-Output ("{0} [{1}] バックアップ整合性検証開始" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                        Invoke-Sqlcmd -ServerInstance $Instance -Query ($VerifySQL -f $BackupFile) -QueryTimeout 1200 -ErrorAction Stop
+                        Write-Output ("{0} [{1}] バックアップ整合性検証終了" -f (Get-Date).ToString("yyyy/MM/dd HH:mm:ss"), $Database.Name)
+                    }
+                }
+            }
+        }catch{
+            throw $_
         }
     }
 }
-
-
-
-Start-SQLServerFullBackup
-Start-SQLServerDiffBackup
-Start-SQLServerLogBackup

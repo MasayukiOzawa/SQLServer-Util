4e4c46d2e5ca2583730757f8c4b2a5b2a5dedcb3
Masayuki Ozawa <masayuki.ozawa@live.jp>
2016-08-13 18:11:38 +0900

    Update DataSet を使用した DB の操作.ps1

--- PowerShell/DataSet を使用した DB の操作.ps1
+++ PowerShell/DataSet を使用した DB の操作.ps1
@@ -26,6 +26,57 @@ foreach($row in $ds.tables[0].Rows){
 
 Write-Output $count
 
+# 関数からの Fill の呼び出し
+function Get-FillData([System.Data.SqlClient.SqlConnection]$connection){
+    $cmd = $connection.CreateCommand()
+    $cmd.CommandText = "SELECT object_id, name FROM sys.objects where object_id < 100"
+
+    $da = New-Object System.Data.SqlClient.SqlDataAdapter
+    $da.SelectCommand = $cmd
+
+    $ds = New-Object System.Data.DataSet
+    # 取得した件数を呼び出し側に返さないように[void]を設定
+    [void]$da.Fill($ds)
+    # コレクションの型を維持
+    return ,$ds
+    # $ds.tables | Select-Object -ExcludeProperty Rows
+}
+
+$ret = Get-FillData($con)
+$ret.GetType()
+
+$ret = @(Get-FillData($con))
+$ret.GetType()
+
+# [void]をいつも忘れる
+# http://d.hatena.ne.jp/ps1/20081119/p1
+# PowerShell データベースから取得した値(DataRow)でハマった
+# https://hinokinobou.wordpress.com/2013/12/20/powershell-%E3%83%87%E3%83%BC%E3%82%BF%E3%83%99%E3%83%BC%E3%82%B9%E3%81%8B%E3%82%89%E5%8F%96%E5%BE%97%E3%81%97%E3%81%9F%E5%80%A4datarow%E3%81%A7%E3%83%8F%E3%83%9E%E3%81%A3%E3%81%9F/
+# PowerShell] 関数でコレクションのインスタンスを返り値とする場合にコレクションの型を維持させる
+# http://www.pine4.net/Memo/Article/Archives/203
+
+function Get-FillData([System.Data.SqlClient.SqlConnection]$connection){
+    $cmd = $connection.CreateCommand()
+    $cmd.CommandText = "SELECT object_id, name FROM sys.objects where object_id = 3"
+
+    $da = New-Object System.Data.SqlClient.SqlDataAdapter
+    $da.SelectCommand = $cmd
+
+    $dt = New-Object System.Data.DataTable
+    # 取得した件数を呼び出し側に返さないように[void]を設定
+    [void]$da.Fill($dt)
+    # コレクションの型を維持
+    return ,$dt
+    return $dt
+}
+
+$ret = Get-FillData($con)
+$ret.GetType()
+
+$ret = @(Get-FillData($con))
+$ret.GetType()
+
+
 # Bulk Copy (CSV)
 # https://gallery.technet.microsoft.com/scriptcenter/4208a159-a52e-4b99-83d4-8048468d29dd
 <#

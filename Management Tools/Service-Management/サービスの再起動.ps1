9eb6d6e3f5ad7520eb4d9d9410d8a5180f16c7b1
Masayuki Ozawa <masayuki.ozawa@live.jp>
2016-11-04 11:20:07 +0900

    Create サービスの再起動.ps1

---
+++ Management Tools/Service-Management/サービスの再起動.ps1
@@ -0,0 +1,15 @@
+$ServiceName = "MSSQLSERVER"
+
+# 依存関係のあるサービスの停止
+$DependentServices = Get-Service $ServiceName -DependentServices | ? Status -ne "Stopped"
+$DependentServices | %{Write-Host "Stop Service [$($_.Name)]";$_ | Stop-Service}
+
+# サービスの停止
+Write-Host "Stop Servcie [$($ServiceName)]";Stop-Service -Name $ServiceName -Force
+
+# サービスの開始
+Write-Host "Start Servcie [$($ServiceName)]";Start-Service -Name $ServiceName
+
+# 依存関係のあるサービスの開始
+$DependentServices = Get-Service "MSSQLSERVER" -DependentServices | ? Status -eq "Stopped"
+$DependentServices |  %{Write-Host "Start Service [$($_.Name)]";$_ | Start-Service}

74549f8ab6430ebbb93f4b427e163c8558db6c82
Masayuki Ozawa <masayuki.ozawa@live.jp>
2016-11-27 19:45:26 +0900

    列ストアのメモリ取得のフォルダの移動とIn-Memory OLTP のメモリ取得を追加

---
+++ Buffer Cache/In-MemoryOLTP.で使用しているメモリの取得.sql
@@ -0,0 +1,24 @@
+﻿SELECT 
+	OBJECT_NAME(mc.object_id) AS object_name,
+	si.name,
+	mc.memory_consumer_desc,
+	mc.memory_consumer_type_desc,
+	SUM(mc.allocated_bytes) TotalAllocatedBytes,
+	SUM(mc.used_bytes) TotalUsedBytes,
+	SUM(mc.allocation_count) TotalAllocationCount
+FROM 
+	sys.dm_db_xtp_memory_consumers mc
+	LEFT JOIN
+	sys.indexes si
+	ON
+	si.object_id = mc.object_id
+	AND
+	si.index_id = mc.index_id
+WHERE
+	mc.object_id >0
+GROUP BY
+	OBJECT_NAME(mc.object_id),
+	si.name,
+	mc.memory_consumer_desc,
+	mc.memory_consumer_type_desc
+OPTION (RECOMPILE)

4175142d21c9656481c57c35d34a936461aafafc
masayuki.ozawa <masayuki.ozawa@live.jp>
2016-11-30 16:57:03 +0900

    DMV 版を追加

--- Database/ファイル IO の取得.sql
+++ Database/ファイル IO の取得.sql
@@ -1,4 +1,4 @@
-SET NOCOUNT ON
+﻿SET NOCOUNT ON
 GO
 
 USE [master]
@@ -28,7 +28,35 @@ FROM
 		AND
 		fn_virtualfilestats.FileId = [sys].[master_files].[file_id]
 OPTION (RECOMPILE)
-		
+
+
+
+/*********************************************/
+-- sys.dm_io_virtual_file_stats
+/*********************************************/
+SELECT
+	GETDATE() AS [DateTime],
+	DB_NAME(mf.database_id) AS DatabaseName, 
+	mf.name, 
+	mf.physical_name, 
+	fs.num_of_reads,
+	fs.io_stall_read_ms,
+	fs.num_of_bytes_read, 
+	fs.num_of_writes, 
+	fs.io_stall_write_ms,
+	fs.num_of_bytes_written, 
+	fs.size_on_disk_bytes
+FROM
+	sys.dm_io_virtual_file_stats(NULL, NULL) fs
+	LEFT JOIN
+	sys.master_files mf  WITH (NOLOCK)
+	ON
+	fs.database_id = mf.database_id
+	AND
+	fs.file_id = mf.file_id
+OPTION (RECOMPILE)		
+
+
 /*********************************************/
 -- IO リクエストの待ちの発生状況の取得
 /*********************************************/

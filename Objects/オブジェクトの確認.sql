218a347c02353f5b8aded063e8b8fe78b95c2506
Masayuki.Ozawa <masayuki.ozawa@live.jp>
2016-12-24 11:53:49 +0900

    クエリを修正

---
+++ Objects/オブジェクトの確認.sql
@@ -0,0 +1,33 @@
+﻿SELECT 
+	ao.name, 
+	ao.type_desc, 
+	ao.type,
+	ao.create_date,
+	ao.modify_date,
+	asm.definition,
+	asm.*,
+	-- インデックス付きビューの SET の条件についての確認
+	CASE 
+		WHEN type = 'P' THEN 
+			CASE
+				WHEN  asm.uses_ansi_nulls = 1  AND asm.uses_quoted_identifier = 1 THEN 1
+				ELSE 0
+			END
+		WHEN type = 'V' THEN 
+			CASE
+				WHEN  asm.uses_ansi_nulls = 1 AND asm.uses_quoted_identifier = 1 AND asm.is_schema_bound = 1 THEN 1
+				ELSE 0
+			END		
+		ELSE NULL
+	END AS indexd_view_requirements
+FROM
+	sys.all_objects ao
+	LEFT JOIN
+	sys.all_sql_modules asm
+	ON
+	ao.object_id = asm.object_id
+WHERE 
+	is_ms_shipped = 0
+ORDER BY name ASC
+
+
--- Query/クエリの進行状況を取得.sql
+++ Query/クエリの進行状況を取得.sql
@@ -1,7 +1,9 @@
--- SET STATISTICS PROFILE ON / SET STATISTICS XML ON を有効にしているクエリに関して情報を取得
+﻿-- SET STATISTICS PROFILE ON / SET STATISTICS XML ON を有効にしているクエリに関して情報を取得
 -- 拡張イベントの query_post_execution_showplan を有効にすることで、個別のクエリで SET 句を付与しなくても取得できるが、拡張イベントを使用する場合はパフォーマンスの劣化に注意する
 -- SQL Server 2014 SP2 / SQL Server 2016 SP1 以降は TF7412 を使用することで、軽量プロファイルを用いた情報の取得が可能
 -- https://blogs.msdn.microsoft.com/sql_server_team/query-progress-anytime-anywhere/ 
+-- DBCC TRACEON(7412, -1)
+
 SELECT
     qp.session_id, 
     qp.request_id, 
@@ -27,5 +29,7 @@ FROM
     sys.dm_os_tasks ot
 ON
     qp.task_address = ot.task_address
+WHERE
+	qp.session_id <> @@SPID
 ORDER BY
     session_id, request_id, node_id, thread_id

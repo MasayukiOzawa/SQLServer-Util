8258eca0ca40c77f1a2275b493f49fa2790908cc
Masayuki Ozawa <masayuki.ozawa@live.jp>
2017-01-27 12:31:38 +0900

    Update クエリによるパフォーマンス情報の取得.sql

--- Performance Monitor/クエリによるパフォーマンス情報の取得.sql
+++ Performance Monitor/クエリによるパフォーマンス情報の取得.sql
@@ -1,4 +1,4 @@
-﻿-- https://msdn.microsoft.com/ja-JP/library/ms191246.aspx
+-- https://msdn.microsoft.com/ja-JP/library/ms191246.aspx
 
 -- CPU 使用率の取得
 SELECT 
@@ -22,3 +22,12 @@ FROM
 WHERE 
 	pr.object_name = 'SQLServer:Resource Pool Stats' AND pr.counter_name = 'CPU usage %'
 
+-- バッチ実行状況の取得 
+DECLARE @previous_time datetime, @current_time datetime
+DECLARE @previous_count bigint, @current_count bigint
+
+SELECT @previous_time = GETDATE(), @previous_count = cntr_value from sys.dm_os_performance_counters where counter_name ='Batch Requests/sec'
+WAITFOR DELAY '00:00:01'
+SELECT @current_time = GETDATE(), @current_count = cntr_value from sys.dm_os_performance_counters where counter_name ='Batch Requests/sec'
+
+SELECT CAST((@current_count - @previous_count ) / (DATEDIFF(ms, @previous_time, @current_time) / 1000.0) AS bigint) AS batch_request

041eaa420d4bfadec3be6b0e6b189cc01bc49bbf
Masayuki Ozawa <masayuki.ozawa@live.jp>
2017-01-27 13:10:36 +0900

    Update クエリによるパフォーマンス情報の取得.sql

--- Performance Monitor/クエリによるパフォーマンス情報の取得.sql
+++ Performance Monitor/クエリによるパフォーマンス情報の取得.sql
@@ -1,26 +1,55 @@
 -- https://msdn.microsoft.com/ja-JP/library/ms191246.aspx
 
 -- CPU 使用率の取得
-SELECT 
-	pr.object_name,
-	'CPU usage %' AS counter_name,
-	pr.instance_name,
-	CASE
-		WHEN pr.cntr_value = 0 THEN 0
-		ELSE CAST((CAST(pr.cntr_value AS FLOAT) / pb.cntr_value) * 100 AS numeric(5,2))
-	END AS 'cpu_usage'                                                                                           
-FROM 
-	sys.dm_os_performance_counters pr
-	INNER JOIN
-	sys.dm_os_performance_counters pb
-	ON
-	pr.object_name = pb.object_name
-	AND
-	pr.instance_name = pb.instance_name
-	AND
-	pb.counter_name = 'CPU usage % base'
-WHERE 
-	pr.object_name = 'SQLServer:Resource Pool Stats' AND pr.counter_name = 'CPU usage %'
+IF CHARINDEX('SQL Azure', CAST(SERVERPROPERTY('Edition') AS nvarchar(255))) = 0
+BEGIN
+	SELECT 
+		RTRIM(pr.object_name) AS object_name,
+		'CPU usage %' AS counter_name,
+		RTRIM(pr.instance_name) AS instance_name,
+		CASE
+			WHEN pr.cntr_value = 0 THEN 0
+			ELSE CAST((CAST(pr.cntr_value AS FLOAT) / pb.cntr_value) * 100 AS numeric(5,2))
+		END AS 'cpu_usage'                                                                                           
+	FROM 
+		sys.dm_os_performance_counters pr
+		INNER JOIN
+		sys.dm_os_performance_counters pb
+		ON
+		pr.object_name = pb.object_name
+		AND
+		pr.instance_name = pb.instance_name
+		AND
+		pb.counter_name = 'CPU usage % base'
+	WHERE 
+		pr.object_name like '%Resource Pool Stats%' AND pr.counter_name = 'CPU usage %'
+		And
+		pr.instance_name = 'default'
+END
+ELSE
+BEGIN
+	SELECT 
+		RTRIM(pr.object_name) AS object_name,
+		'CPU usage %' AS counter_name,
+		RTRIM(pr.instance_name) AS instance_name,
+		CASE
+			WHEN pr.cntr_value = 0 THEN 0
+			ELSE CAST((CAST(pr.cntr_value AS FLOAT) / pb.cntr_value) * 100 AS numeric(5,2))
+		END AS 'cpu_usage'                                                                                           
+	FROM 
+		sys.dm_os_performance_counters pr
+		INNER JOIN
+		sys.dm_os_performance_counters pb
+		ON
+		pr.object_name = pb.object_name
+		AND
+		pr.instance_name = pb.instance_name
+		AND
+		pb.counter_name = 'CPU usage % base'
+	WHERE 
+		pr.object_name like '%Resource Pool Stats%' AND pr.counter_name = 'CPU usage %'
+
+END
 
 -- バッチ実行状況の取得 
 DECLARE @previous_time datetime, @current_time datetime

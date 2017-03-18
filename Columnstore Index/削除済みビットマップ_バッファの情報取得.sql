2fdef496ca2da6d28672493f5af4d27bc6ed5fcb
Masayuki.Ozawa <masayuki.ozawa@live.jp>
2017-03-18 23:18:21 +0900

    no message

---
+++ Columnstore Index/削除済みビットマップ_バッファの情報取得.sql
@@ -0,0 +1,86 @@
+﻿-- w削除済みバッファ / ビットマップの情報取得
+SELECT 
+	OBJECT_NAME(ip.object_id) object_name,
+	i.name,
+	ip.partition_number,
+	ip.internal_object_type_desc,
+	ip.row_group_id,
+	ip.rows,
+	ip.data_compression_desc,
+	au.type_desc,
+	au.data_space_id,
+	au.total_pages,
+	au.used_pages,
+	au.data_pages
+FROM 
+	sys.internal_partitions ip
+	LEFT JOIN
+	sys.indexes i
+	ON
+	i.object_id = ip.object_id
+	AND
+	i.index_id = ip.index_id
+	LEFT JOIN
+	sys.allocation_units au
+	ON
+	ip.hobt_id = au.container_id
+WHERE
+	rows > 0
+ORDER BY
+	OBJECT_NAME(ip.object_id),
+	ip.partition_number
+
+-- DMV から割り当てページを確認
+SELECT 
+	DB_NAME(pa.database_id) AS database_name,
+	OBJECT_NAME(pa.object_id) AS object_name,
+	i.name,
+	p.internal_object_type_desc,
+	p.rows,
+	p.data_compression_desc,
+	pa.partition_id,
+	pa.allocated_page_file_id,
+	pa.extent_page_id,
+	pa.allocated_page_page_id,
+	pa.allocation_unit_type_desc,
+	pa.page_free_space_percent,
+	pa.is_page_compressed,
+	pa.allocated_page_iam_page_id,
+	pa.rowset_id,
+	pa.allocation_unit_id,
+	pa.is_iam_page,
+	pa.is_mixed_page_allocation,
+	pa.next_page_file_id,
+	pa.next_page_page_id,
+	pa.previous_page_file_id,
+	pa.previous_page_page_id
+
+FROM
+	sys.internal_partitions p
+	LEFT JOIN
+	sys.dm_db_database_page_allocations(
+		DB_ID(), 
+		OBJECT_ID('T1'), 
+		NULL, -- index_id
+		NULL, -- partition_id
+		'DETAILED' -- 'LIMITED'
+	) pa
+	ON
+	pa.object_id = p.object_id
+	AND
+	pa.rowset_id = p.hobt_id
+	INNER JOIN
+	sys.indexes i
+	ON
+	i.object_id = pa.object_id
+	AND
+	i.index_id = pa.index_id
+
+-- ページ情報
+DBCC TRACEON(3604)
+DBCC PAGE(N'tpch', 1, 66408, 3) WITH TABLERESULTS
+
+
+-- ALTER INDEX ALL ON T1 REORGANIZE
+-- DELETE TOP (1) FROM T1
+
--- Page/ページ情報の確認.sql
+++ Page/ページ情報の確認.sql
@@ -1,4 +1,4 @@
-SET NOCOUNT ON
+﻿SET NOCOUNT ON
 GO
 -- テスト用のテーブル作成
 DROP TABLE IF EXISTS T1
@@ -54,6 +54,7 @@ GO
 CHECKPOINT
 GO
 
+/********************************************************************************************************/
 -- データからページ番号を取得する
 -- ページ番号が正しくない場合、sys.dm_os_buffer_descriptors から情報を取得する
 DBCC DROPCLEANBUFFERS
@@ -62,9 +63,11 @@ GO
 SELECT * FROM T1 CROSS APPLY sys.fn_PhysLocCracker(%%physloc%%) WHERE C1 BETWEEN 1 AND  200000
 SELECT * FROM T2 CROSS APPLY sys.fn_PhysLocCracker(%%physloc%%) WHERE C1 BETWEEN 1 AND  200000
 GO
+/********************************************************************************************************/
 
 
--- fn_PhysLocCracker から正しいページ番号を取得できなかった場合、キャッシュの情報から取得する
+/********************************************************************************************************/
+-- fn_PhysLocCracker から正しいページ番号を取得できなかった場合、キャッシュの情報から取得することも可能
 SELECT   
 	db_name(database_id) AS Database_name,  
 	OBJECT_NAME(p.object_id) AS object_name,  
@@ -110,7 +113,47 @@ ORDER BY
 	page_id,
 	page_level
 GO
+/********************************************************************************************************/
 
+
+/********************************************************************************************************/
+-- DMV から割り当てページを確認
+SELECT 
+	DB_NAME(pa.database_id) AS database_name,
+	OBJECT_NAME(pa.object_id) AS object_name,
+	i.name,
+	pa.partition_id,
+	pa.allocated_page_file_id,
+	pa.extent_page_id,
+	pa.allocated_page_page_id,
+	pa.allocation_unit_type_desc,
+	pa.page_free_space_percent,
+	pa.is_page_compressed,
+	pa.allocated_page_iam_page_id,
+	pa.rowset_id,
+	pa.allocation_unit_id,
+	pa.is_iam_page,
+	pa.is_mixed_page_allocation,
+	pa.next_page_file_id,
+	pa.next_page_page_id,
+	pa.previous_page_file_id,
+	pa.previous_page_page_id
+
+FROM 
+	sys.dm_db_database_page_allocations(
+		DB_ID(), 
+		OBJECT_ID('T1'), 
+		NULL, -- index_id
+		NULL, -- partition_id
+		'DETAILED' -- 'LIMITED'
+	) pa
+	INNER JOIN
+	sys.indexes i
+	ON
+	i.object_id = pa.object_id
+	AND
+	i.index_id = pa.index_id
+/********************************************************************************************************/
 
 -- ページの実情報の確認
 DBCC TRACEON(3604)

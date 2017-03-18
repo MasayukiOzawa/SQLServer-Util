156f05df8fa271d68e9dcf9ad5aae6725caf4553
Masayuki Ozawa <masayuki.ozawa@live.jp>
2017-03-18 23:30:27 +0900

    no message

---
+++ PowerShell/Management/Windows ファイアウォールの設定.ps1
@@ -0,0 +1,3 @@
+﻿New-NetFirewallRule -Group "SQL Server" -Name "SQL Server" -DisplayName "SQL Server" -Protocol tcp -LocalPort @(1433, 1434)
+New-NetFirewallRule -Group "SQL Server" -Name "SQL Server Browser" -DisplayName "SQL Server Browser" -Protocol udp -LocalPort @(1434)
+
---
+++ PowerShell/Management/ポート番号の変更.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param(
+[string]$NewPort = "1433"
+)
+
+$ServiceName = (Get-WmiObject -Class win32_service | where PathName -match "sqlservr.exe").Name
+$InstanceName = ($ServiceName -split "\$")[-1]
+
+Import-Module SQLPS -DisableNameChecking
+
+$MachineObject = new-object ("Microsoft.SqlServer.Management.Smo.WMI.ManagedComputer")
+$ProtocolUri = "ManagedComputer[@Name='" + (get-item env:computername).Value + "']/ServerInstance[@Name='$InstanceName']/ServerProtocol"
+
+$tcp = $MachineObject.getsmoobject($ProtocolUri + "[@Name='Tcp']")
+
+$ipall = $MachineObject.getsmoobject($tcp.urn.value + "/IPAddress[@Name='IPAll']")
+$ipall.IPAddressProperties["TcpPort"].Value = $NewPort
+$tcp.Alter()
+
+Restart-Service $Servicename -Force
+Get-NetTCPConnection -LocalPort $NewPort

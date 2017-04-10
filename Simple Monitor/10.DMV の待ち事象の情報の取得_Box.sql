bca52bff2903ca64da97e0df3e52396a1ba78469
Masayuki Ozawa <masayuki.ozawa@live.jp>
2017-04-10 23:41:59 +0900

    待ち事象の情報取得を追加

---
+++ Simple Monitor/10.DMV の待ち事象の情報の取得_Box.sql
@@ -0,0 +1,36 @@
+﻿DROP TABLE IF EXISTS #T1
+DROP TABLE IF EXISTS #T2
+
+SELECT
+	GETDATE() AS counter_date,
+	*
+INTO #T1
+FROM
+	sys.dm_os_wait_stats
+
+WAITFOR DELAY '00:00:01'
+
+SELECT
+	GETDATE() AS counter_date,
+	*
+INTO #T2
+FROM
+	sys.dm_os_wait_stats
+
+SELECT
+	#T2.counter_date,
+	#T2.wait_type,
+	#T2.waiting_tasks_count - #T1.waiting_tasks_count AS waiting_tasks_count,
+	#T2.wait_time_ms - #T1.wait_time_ms AS wait_time_ms,
+	#T2.max_wait_time_ms,
+	#T2.signal_wait_time_ms - #T1.signal_wait_time_ms AS signal_wait_time_ms
+FROM
+	#T2
+	LEFT JOIN
+	#T1
+	ON
+	#T1.wait_type = #T2.wait_Type
+WHERE
+	#T2.waiting_tasks_count - #T1.waiting_tasks_count > 0
+	AND
+	#T2.wait_type <> 'WAITFOR'

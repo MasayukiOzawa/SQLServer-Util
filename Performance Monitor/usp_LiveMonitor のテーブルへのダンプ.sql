b5f4c6b8160a12c186385ab22fb5d13bee4a2714
Masayuki Ozawa <masayuki.ozawa@live.jp>
2017-05-28 23:10:17 +0900

    usp_LiveMonitor を追加

---
+++ Performance Monitor/usp_LiveMonitor のテーブルへのダンプ.sql
@@ -0,0 +1,40 @@
+﻿SET NOCOUNT ON
+GO
+
+DROP TABLE IF EXISTS PerfTable
+GO
+
+CREATE TABLE PerfTable(
+	[Counter Date] datetime2 ,
+	[Server Name] sysname ,
+	[CPU Usage %] numeric(5,2) ,
+	[Buffer Cache Hit %] numeric(5,2) ,
+	[Plan Cache Hit %] numeric(5,2) ,
+	[Page life expectancy] bigint ,
+	[Database Cache Memory (MB)] float ,
+	[Plan Chache Memory (MB)] float ,
+	[Free Memory (MB)] float ,
+	[Total Server Memory (MB)] float ,
+	[Target Server Memory (MB)] float ,
+	[Granted Workspace Memory (MB)] float ,
+	[Memory Grants Outstanding] bigint ,
+	[Memory Grants Pending] bigint ,
+	[Batch Requests/sec] bigint ,
+	[Page lookups (MB)/sec] float ,
+	[Readahead pages (MB)/sec] float ,
+	[Page reads (MB)/sec] float ,
+	[Page writes (MB)/sec] float ,
+	[Checkpoint pages (MB)/sec] float ,
+	[Background writer pages (MB)/sec] float ,
+	[Log Flushes/sec] bigint ,
+	[Log MBytes Flushed/sec] float ,
+	[Log Flush Waits/sec] bigint ,
+	[Log Flush Wait Time] bigint ,
+	[SQL Compilations/sec] bigint ,
+	[SQL Re-Compilations/sec] bigint
+)
+GO
+
+INSERT INTO PerfTable EXEC usp_livemonitor 
+WAITFOR DELAY '00:00:05'
+GO 1000

085579d91db454cd2a95deb5d26ed3e39a02430f
Masayuki Ozawa <masayuki.ozawa@live.jp>
2017-12-15 18:42:30 +0900

    実行中のクエリ情報の取得クエリを追加

---
+++ Instance/SQL Server の利用状況モニター相当の情報を取得するためのクエリ v2.sql
@@ -0,0 +1,52 @@
+﻿
+WITH profiled_sessions as (
+	SELECT DISTINCT session_id profiled_session_id from sys.dm_exec_query_profiles
+)
+SELECT 
+   [Session ID]    = s.session_id, 
+   [User Process]  = CONVERT(CHAR(1), s.is_user_process),
+   [Login]         = s.login_name,   
+   [Database]      = case when p.dbid=0 then N'' else ISNULL(db_name(p.dbid),N'') end, 
+   [Task State]    = ISNULL(t.task_state, N''), 
+   [Command]       = ISNULL(r.command, N''), 
+   [Application]   = ISNULL(s.program_name, N''), 
+   [Wait Time (ms)]     = ISNULL(w.wait_duration_ms, 0),
+   [Wait Type]     = ISNULL(w.wait_type, N''),
+   [Wait Resource] = ISNULL(w.resource_description, N''), 
+   [Blocked By]    = ISNULL(CONVERT (varchar, w.blocking_session_id), ''),
+   [Head Blocker]  = 
+        CASE 
+            -- session has an active request, is blocked, but is blocking others or session is idle but has an open tran and is blocking others
+            WHEN r2.session_id IS NOT NULL AND (r.blocking_session_id = 0 OR r.session_id IS NULL) THEN '1' 
+            -- session is either not blocking someone, or is blocking someone but is blocked by another party
+            ELSE ''
+        END, 
+   [Total CPU (ms)] = s.cpu_time, 
+   [Total Physical I/O (MB)]   = (s.reads + s.writes) * 8 / 1024, 
+   [Memory Use (KB)]  = s.memory_usage * (8192 / 1024), 
+   [Open Transactions] = ISNULL(r.open_transaction_count,0), 
+   [Login Time]    = s.login_time, 
+   [Last Request Start Time] = s.last_request_start_time,
+   [Host Name]     = ISNULL(s.host_name, N''),
+   [Net Address]   = ISNULL(c.client_net_address, N''), 
+   [Execution Context ID] = ISNULL(t.exec_context_id, 0),
+   [Request ID] = ISNULL(r.request_id, 0),
+   [Workload Group] = ISNULL(g.name, N''),
+   [Profiled Session Id] = profiled_session_id
+FROM sys.dm_exec_sessions s LEFT OUTER JOIN sys.dm_exec_connections c ON (s.session_id = c.session_id)
+LEFT OUTER JOIN sys.dm_exec_requests r ON (s.session_id = r.session_id)
+LEFT OUTER JOIN sys.dm_os_tasks t ON (r.session_id = t.session_id AND r.request_id = t.request_id)
+LEFT OUTER JOIN 
+(
+    -- In some cases (e.g. parallel queries, also waiting for a worker), one thread can be flagged as 
+    -- waiting for several different threads.  This will cause that thread to show up in multiple rows 
+    -- in our grid, which we don't want.  Use ROW_NUMBER to select the longest wait for each thread, 
+    -- and use it as representative of the other wait relationships this thread is involved in. 
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY waiting_task_address ORDER BY wait_duration_ms DESC) AS row_num
+    FROM sys.dm_os_waiting_tasks 
+) w ON (t.task_address = w.waiting_task_address) AND w.row_num = 1
+LEFT OUTER JOIN sys.dm_exec_requests r2 ON (s.session_id = r2.blocking_session_id)
+LEFT OUTER JOIN sys.dm_resource_governor_workload_groups g ON (g.group_id = s.group_id)
+LEFT OUTER JOIN sys.sysprocesses p ON (s.session_id = p.spid)
+LEFT OUTER JOIN profiled_sessions ON profiled_session_id = s.session_id
+ORDER BY s.session_id;
---
+++ Query/実行中のクエリのスレッド情報の取得.sql
@@ -0,0 +1,129 @@
+﻿SELECT 
+	t.session_id,
+	wg.name,
+	DB_NAME(r.database_id) AS database_name,
+	s.login_name,
+	USER_NAME(r.user_id) AS user_name,
+	s.is_user_process,
+	s.login_time,
+	r.start_time,
+	r.command,
+	tx.text,
+	r.last_wait_type,
+	r.wait_resource,
+	t.task_state,
+	w.state,
+	w.last_wait_type,
+	s.host_name,
+	s.program_name,
+	s.client_version,
+	s.client_interface_name,
+	s.login_name,
+	s.nt_domain,
+	s.nt_user_name,
+	s.status,
+	s.cpu_time,
+	s.memory_usage,
+
+	s.total_scheduled_time,
+	s.total_elapsed_time,
+	s.last_request_start_time,
+	s.last_request_end_time,
+
+	s.reads,
+	s.writes,
+	s.logical_reads,
+	s.row_count,
+	r.status,
+	r.wait_time,
+	r.cpu_time,
+	r.total_elapsed_time,
+	r.reads,
+	r.writes,
+	r.logical_reads,
+
+	t.pending_io_count,
+	w.pending_io_count,
+	w.pending_io_byte_count,
+	t.pending_io_byte_count,
+	w.pending_io_byte_count,
+	t.pending_io_byte_average,
+	w.pending_io_byte_average,
+
+	mg.grant_time,
+	mg.requested_memory_kb,
+	mg.granted_memory_kb,
+	mg.required_memory_kb,
+	mg.used_memory_kb,
+	mg.max_used_memory_kb,
+	mg.query_cost,
+
+	mg.scheduler_id,
+	t.scheduler_id,
+	mg.request_id,
+	t.request_id,
+	r.dop,
+	mg.dop,
+	r.parallel_worker_count,
+	mg.reserved_worker_count,
+	mg.used_worker_count,
+	mg.max_used_worker_count,
+	r.blocking_session_id,
+	th.kernel_time,
+	th.usermode_time,
+
+	t.context_switches_count,
+	w.context_switch_count,
+	
+	th.creation_time,
+	th.started_by_sqlservr,
+
+	s.host_process_id,
+	th.os_thread_id,
+	th.thread_address,
+	th.worker_address,
+	th.status,
+	w.wait_started_ms_ticks
+
+FROM 
+	sys.dm_os_tasks AS t
+	LEFT JOIN
+	sys.dm_os_threads AS th
+	ON
+	th.worker_address = t.worker_address
+	LEFT JOIN
+	sys.dm_os_workers AS w
+	ON
+	w.worker_address = t.worker_address
+	LEFT JOIN
+	sys.dm_exec_requests AS r
+	ON
+	r.session_id = t.session_id
+	AND
+	r.request_id = t.request_id
+	LEFT JOIN
+	sys.dm_exec_sessions AS s
+	ON
+	s.session_id = t.session_id
+	LEFT JOIN
+	sys.dm_exec_query_memory_grants AS mg
+	ON
+	mg.session_id = r.session_id
+	AND
+	mg.request_id = r.request_id
+	LEFT JOIN
+	sys.resource_governor_workload_groups AS wg
+	ON
+	r.group_id = wg.group_id
+	OUTER APPLY sys.dm_exec_sql_text(r.sql_handle) AS tx
+WHERE 
+	t.session_id IS NOT NULL
+	AND
+	t.session_id <> @@SPID
+	AND
+	s.is_user_process = 1
+ORDER BY
+	t.session_id,
+	t.scheduler_id
+OPTION(RECOMPILE)
+

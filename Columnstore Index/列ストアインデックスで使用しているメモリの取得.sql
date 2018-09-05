3be541ea27a69cbb908c6d64019be0213b3da5e5
Masayuki Ozawa <masayuki.ozawa@live.jp>
2018-09-05 11:13:03 +0900

    列ストアインデックスのメモリ情報の取得を追加

---
+++ Columnstore Index/列ストアインデックスで使用しているメモリの取得.sql
@@ -0,0 +1,3 @@
+﻿SELECT * FROM sys.dm_os_memory_broker_clerks WHERE clerk_name = 'Column store object pool'
+SELECT SUM(pages_in_bytes) / POWER(1024,2) FROM sys.dm_os_memory_objects WHERE type LIKE '%COLUMNSTORE%'
+SELECT * FROM sys.dm_os_memory_clerks WHERE type LIKE '%COLUMNSTORE%'

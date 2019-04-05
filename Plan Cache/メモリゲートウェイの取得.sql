ff5f034f11e51f3049fb4499be75977b54068b7b
Masayuki Ozawa <masayuki.ozawa@live.jp>
2019-04-05 17:53:53 +0900

    メモリゲートウェイの情報取得を追加

---
+++ Plan Cache/メモリゲートウェイの取得.sql
@@ -0,0 +1,18 @@
+﻿-- https://support.microsoft.com/ja-jp/help/3024815/large-query-compilation-waits-on-resource-semaphore-query-compile-in-s
+-- -T6498
+SELECT 
+	MemGW.pool_id,
+	RP.name,
+	MemGW.name AS gateay_name,
+	MemGW.max_count,
+	MemGW.active_count,
+	MemGW.waiter_count,
+	MemGW.threshold_factor,
+	MemGW.threshold,
+	MemGW.is_active
+FROM 
+	sys.dm_exec_query_optimizer_memory_gateways AS MemGW
+	LEFT JOIN
+	sys.resource_governor_resource_pools AS RP
+	ON
+	MemGW.pool_id = RP.pool_id

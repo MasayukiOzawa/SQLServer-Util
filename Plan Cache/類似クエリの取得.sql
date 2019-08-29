f7edd5d65927e228cfae5901b2b6a81376dcc97c
Masayuki Ozawa <masayuki.ozawa@live.jp>
2019-08-29 16:25:39 +0900

    Update 類似クエリの取得.sql

--- Plan Cache/類似クエリの取得.sql
+++ Plan Cache/類似クエリの取得.sql
@@ -1,12 +1,13 @@
-﻿/*************************************************
+/*************************************************
 SQL Server 2008 R2 以降
 *************************************************/
 SET NOCOUNT ON
 GO
-
+-- 複数キャッシュされている類似クエリの query_hash を取得
 SELECT
     GETDATE() AS DATE,
     REPLACE(REPLACE(REPLACE(MAX(est.text),CHAR(13), ''), CHAR(10), ' '),CHAR(9), ' ') AS text
+	, objtype
     , COUNT(*) AS count
     , query_hash
     , query_plan_hash
@@ -14,12 +15,16 @@ FROM
     sys.dm_exec_query_stats eqs
     OUTER APPLY
     sys.dm_exec_sql_text(sql_handle) est
+	LEFT JOIN sys.dm_exec_cached_plans AS cp
+	ON cp.plan_handle = eqs.plan_handle
 GROUP BY
     query_hash
     , query_plan_hash
+	, cp.objtype
 HAVING
     COUNT(*) > 1
 ORDER BY
     count DESC
     , text
 OPTION (RECOMPILE)
+GO

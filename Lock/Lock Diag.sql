93bba58631c948a381a375abac5a47bded9b1fb2
masayuki_ozawa <masayuki.ozawa@live.jp>
2019-10-27 12:45:20 +0900

    no message

---
+++ Lock/Lock Diag.sql
@@ -0,0 +1,39 @@
+﻿/*
+DROP TABLE IF EXISTS LockInfo
+GO
+*/
+SELECT
+	*
+FROM(
+	SELECT
+		l.name,
+		l.timestamp AT TIME ZONE 'Tokyo Standard Time' AS timestamp_jst,
+		DB_NAME(l.database_id) AS database_name,
+		l.resource_type,
+		l.mode,
+		CASE 
+			WHEN resource_type IN ('KEY', 'PAGE', 'RID', 'HOBT') THEN OBJECT_SCHEMA_NAME(p.object_id, l.database_id)
+			WHEN resource_type = 'OBJECT' THEN OBJECT_SCHEMA_NAME(l.object_id, l.database_id)
+			ELSE NULL
+		END AS schema_name,
+		CASE 
+			WHEN resource_type IN ('KEY', 'PAGE', 'RID', 'HOBT') THEN OBJECT_NAME(p.object_id, l.database_id)
+			WHEN resource_type = 'OBJECT' THEN OBJECt_NAME(l.object_id, l.database_id)
+			WHEN resource_type = 'DATABASE' THEN DB_NAME(l.database_id)
+			ELSE NULL
+		END AS object_name,
+		l.owner_type,
+		l.session_id,
+		l.sql_text,
+		l.client_app_name
+	FROM
+		LockInfo AS l
+		LEFT JOIN
+		sys.partitions AS p
+		ON hobt_id = associated_object_id
+	WHERE
+		l.name IN('lock_acquired', 'lock_released')
+) AS T
+ORDER BY
+	timestamp_jst ASC
+
---
+++ Lock/Lock Event.sql
@@ -0,0 +1,10 @@
+﻿CREATE EVENT SESSION [LockInfo] ON SERVER 
+ADD EVENT sqlserver.lock_acquired(
+    ACTION(sqlserver.client_app_name,sqlserver.database_id,sqlserver.database_name,sqlserver.is_system,sqlserver.nt_username,sqlserver.query_hash,sqlserver.query_hash_signed,sqlserver.query_plan_hash,sqlserver.query_plan_hash_signed,sqlserver.session_id,sqlserver.sql_text,sqlserver.username)),
+ADD EVENT sqlserver.lock_released(
+    ACTION(sqlserver.client_app_name,sqlserver.database_id,sqlserver.database_name,sqlserver.is_system,sqlserver.nt_username,sqlserver.query_hash,sqlserver.query_hash_signed,sqlserver.query_plan_hash,sqlserver.query_plan_hash_signed,sqlserver.session_id,sqlserver.sql_text,sqlserver.username))
+ADD TARGET package0.event_file(SET filename=N'LockInfo',max_file_size=(500),max_rollover_files=(20))
+WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=NO_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=ON,STARTUP_STATE=OFF)
+GO
+
+

8b7f66559862176c2509617efb56b1af343fe264
masayuki_ozawa <masayuki.ozawa@live.jp>
2020-04-29 20:58:10 +0900

    統計情報の可視化用クエリの追加

---
+++ Statistics/統計情報の可視化用.sql
@@ -0,0 +1,34 @@
+﻿use tpch;
+DECLARE @table_name varchar(255) = 'LINEITEM'
+DECLARE @stats_id varchar(5) = 4
+DECLARE @pvt varchar(max) = (
+    SELECT STUFF(C1,1, 2,'') + ']'
+    FROM
+    (
+    SELECT 
+        DISTINCT 
+        '],[' + REPLACE(REPLACE(CAST(range_high_key AS varchar(1000)), CHAR(10),''), CHAR(13), '')
+    from 
+        sys.dm_db_stats_histogram(object_id(@table_name), CAST(@stats_id AS int)) 
+    FOR XML PATH('')
+    ) AS T(C1)
+)
+
+DECLARE @sql nvarchar(max) = N'
+SELECT
+    *
+FROM
+(
+    select 
+        REPLACE(REPLACE(CAST(range_high_key AS varchar(1000)), CHAR(10),''''), CHAR(13), '''') AS range_high_key, 
+        range_rows
+    from 
+        sys.dm_db_stats_histogram(object_id(''' + @table_name + '''),' +  @stats_id + ') 
+) AS T
+PIVOT(
+    MAX(range_rows)
+    FOR range_high_key IN(' + @pvt + ')
+) AS PVT
+'
+
+EXECUTE (@sql)

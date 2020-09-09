242a22df157391f9522a818a7faf76cde7b34926
masayuki_ozawa <masayuki.ozawa@live.jp>
2020-09-09 17:45:11 +0900

    no message

--- Query or Plan Cache/実行プランの XML からプロパティの抽出.sql
+++ Query or Plan Cache/実行プランの XML からプロパティの抽出.sql
@@ -3,6 +3,7 @@ SELECT
     T.*,
     T2.stmt.query('declare default element namespace "http://schemas.microsoft.com/sqlserver/2004/07/showplan";.') AS statementPlan,
     T2.stmt.value('(./@StatementText)[1]','varchar(max)') AS StatementText,
+    T2.stmt.value('(./@StatementOptmEarlyAbortReason)[1]','varchar(max)') AS StatementOptmEarlyAbortReason,
     T2.stmt.value('(./@StatementOptmLevel)[1]','varchar(max)') AS StatementOptmLevel,
     T2.stmt.value('(//QueryPlan/@NonParallelPlanReason)[1]','varchar(max)') AS NonParallelPlanReason,
     FORMAT(T2.stmt.value('(//QueryPlan/@CachedPlanSize)[1]','int'), '#,##0') AS CachedPlanSize_kb,
@@ -15,8 +16,7 @@ SELECT
     FORMAT(T2.stmt.value('(//MemoryGrantInfo/@MaxUsedMemory)[1]','int'), '#,##0') AS MaxUsedMemory_kb
 FROM
 (
--- SELECT plan_id, cast(query_plan as xml) AS query_plan FROM sys.query_store_plan
-SELECT query_hash, query_plan_hash, query_plan FROM sys.dm_exec_query_stats
-CROSS APPLY sys.dm_exec_query_plan(plan_handle)
+SELECT plan_id, cast(query_plan as xml) AS query_plan FROM sys.query_store_plan
+-- SELECT query_hash, query_plan_hash, query_plan FROM sys.dm_exec_query_stats CROSS APPLY sys.dm_exec_query_plan(plan_handle)
 ) AS T
 CROSS APPLY query_plan.nodes('//StmtSimple') AS T2(stmt)
